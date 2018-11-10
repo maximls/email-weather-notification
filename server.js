3cc050fd753f9fb0f38ad57a7c02e2c92f57dfa8
Max Lysakovsky <maxim.lysakovsky@gmail.com>
2018-11-10 16:30:10 -0500

    add views

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { mongoose } = require("./db/mongoose");
 const { User } = require("./models/user");
+var hbs = require("hbs");
 const port = process.env.PORT;
 
 const app = express();
-
+hbs.registerPartials(__dirname + "/views/partials/");
+app.set("view engine", "hbs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.status(200).render("index.hbs");
+});
 
 app.post("/", (req, res) => {
   const user = new User({
@@ -38,7 +44,24 @@ app.post("/", (req, res) => {
   );
 });
 
-app.patch("/update/:email", (req, res) => {
+app.get("/update/:email", (req, res) => {
+  const email = req.params.email;
+  User.findOne({ email })
+    .then(fields => {
+      res.status(200).render("update.hbs", {
+        method: "post",
+        action: "/update",
+        param: email,
+        email: fields.email,
+        location: fields.location,
+        time: fields.time,
+        units: fields.units
+      });
+    })
+    .catch(err => res.status(400).send("Cannot find user"));
+});
+
+app.post("/update/:email", (req, res) => {
   const email = req.params.email;
 
   User.findOneAndUpdate(
@@ -60,15 +83,22 @@ app.patch("/update/:email", (req, res) => {
     .catch(err => res.status(400).send());
 });
 
-// app.delete("/:email", (req, res) => {
-//     const email = req.params.email
-// })
-
-app.get("/find/:email", (req, res) => {
+app.get("/delete/:email", (req, res) => {
   const email = req.params.email;
   User.findOne({ email })
-    .then(user => res.status(200).send(user), err => res.status(400).send(err))
-    .catch(err => res.status(400).send());
+    .then(user => {
+      res.status(200).render("delete.hbs", {
+        email: user.email
+      });
+    })
+    .catch(err => res.status(400).send(err));
+});
+
+app.post("/delete/:email", (req, res) => {
+  const email = req.params.email;
+  User.findOneAndDelete({ email }, { select: email })
+    .then(user => res.status(200).send(`User ${user.email} deleted`))
+    .catch(err => res.status(400).send(err));
 });
 
 app.listen(port, () => {
